aa7b9a31ddeadd21e4d25aa5e3f09e9804b570bc
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-05 15:43:07 -0700

    add 404 and error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const PageNotFound = () => (
+  <Box>
+    <Text>Page not found</Text>
+  </Box>
+);
+export default PageNotFound;
---
+++ src/pages/_error.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const ErrorPage = () => (
+  <Box>
+    <Text>Error</Text>
+  </Box>
+);
+
+export default ErrorPage;
