876031ef0a70e0dab41028a1178718a5c63e33fc
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-05 15:19:19 -0700

    fix style

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -1,16 +1,11 @@
-import {
-    VStack,
-    Button,
-    Heading,
-} from "@chakra-ui/react";
+import { VStack, Heading } from "@chakra-ui/react";
 
 const Preferences = () => {
-
-    return (
-        <VStack spacing={5} align="flex-start" >
-            <Heading>Prof Preferences</Heading>
-        </VStack>
-    )
+  return (
+    <VStack spacing={5} align="flex-start">
+      <Heading>Prof Preferences</Heading>
+    </VStack>
+  );
 };
 
 export default Preferences;
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,26 +1,28 @@
-import {
-    VStack,
-    Button,
-} from "@chakra-ui/react";
-import SampleSidesheet from "../components/Sample/SampleSidesheet";
-import SamplePageHeader from "../components/Sample/SamplePageHeader";
+import { VStack, Button } from "@chakra-ui/react";
 import { useState } from "react";
 
+import SampleSidesheet from "../components/Sample/SampleSidesheet";
+import SamplePageHeader from "../components/Sample/SamplePageHeader";
+
 const Sample = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleSubmit = (values) => {
-        alert(JSON.stringify(values, null, 2));
-        setOpen(false);
-    }
+  const handleSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+    setOpen(false);
+  };
 
-    return (
-        <VStack spacing={5} align="flex-start" >
-            <SamplePageHeader />
-            <Button onClick={() => setOpen(true)}>Open Sidesheet</Button>
-            <SampleSidesheet isOpen={open} onClose={() => setOpen(false)} handleSubmit={handleSubmit} />
-        </VStack>
-    )
+  return (
+    <VStack spacing={5} align="flex-start">
+      <SamplePageHeader />
+      <Button onClick={() => setOpen(true)}>Open Sidesheet</Button>
+      <SampleSidesheet
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        handleSubmit={handleSubmit}
+      />
+    </VStack>
+  );
 };
 
 export default Sample;
