79cb94874e50eb3db70c0776f26efac5c784f3fb
Damandeep Singh Bawa <damandeep2018@gmail.com>
2022-06-09 14:35:29 -0700

    Squashed commit of the following:
    commit 795a8be8f770bcd6a8a405d710e719e0bf18d602
    Merge: 6c9a142 7ee50bd
    Author: Damandeep Singh Bawa <damandeepbawa@gmail.com>
    Date:   Wed Jun 8 11:14:24 2022 -0700
    
        Merge pull request #46 from seng499-company2/PreferencesBoilerplate
    
        Preferences boilerplate
    
    commit 7ee50bd773dd462522e8502e29b131bb3c3f4a60
    Merge: 3ffd30b 6c9a142
    Author: Damandeep Singh Bawa <damandeepbawa@gmail.com>
    Date:   Wed Jun 8 11:04:26 2022 -0700
    
        Merge branch 'dev' into PreferencesBoilerplate
    
    commit 3ffd30b091a1994dbbb578e04dab8e4a315562de
    Author: Damandeep Singh Bawa <damandeep2018@gmail.com>
    Date:   Wed Jun 8 11:00:53 2022 -0700
    
        added boilerplater for general preferences
    
    commit 6c9a1428aa8a26c233a6e8025abd78255cc10070
    Merge: d3ce51c 7003cb2
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Tue Jun 7 16:05:12 2022 -0400
    
        Add CI/CD
    
    commit 7003cb293ce3b338f943360df36d8297edbc1bf7
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 22:57:42 2022 -0700
    
        add ci workflow
    
    commit c5e80b9cdba248048c315bd6bea0dc3727634020
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 22:57:11 2022 -0700
    
        add deployment workflow
    
    commit b0f09df908749c5beca552b0ee092db187a5c392
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 22:55:39 2022 -0700
    
        add dockerfile, dockerignore
    
    commit d3ce51cf487cdcbbefe5b7fb12e179f566d1ad36
    Merge: 6e94b73 18d7fe3
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Tue Jun 7 16:01:41 2022 -0400
    
        Prettier, env setup
    
    commit 18d7fe3d4ef5c9d4d6f008bce155d0e988335772
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 22:45:29 2022 -0700
    
        remove unused imports
    
    commit ecedca701e79cecaf050b2350e5d0cfc5c0ddcd8
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 20:01:23 2022 -0700
    
        add .vscode to gitignore
    
    commit 67de6718bc7a4e81ffbd71ddf317b0f1daf4bc18
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 20:00:27 2022 -0700
    
        format files with prettier
    
    commit 2089b6b3fc1de342c36e19ef18a81efc3c9a3bd1
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 20:00:08 2022 -0700
    
        change prettier tab width to 4
    
    commit 6e94b73122aced860b7bfc31de493901e2234020
    Merge: 5bbe3b5 aa7b9a3
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Mon Jun 6 17:57:49 2022 -0400
    
        Merge pull request #43 from seng499-company2/15-skeleton-navigation
    
        15 skeleton navigation
    
    commit aa7b9a31ddeadd21e4d25aa5e3f09e9804b570bc
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Sun Jun 5 15:43:07 2022 -0700
    
        add 404 and error pages
    
    commit 876031ef0a70e0dab41028a1178718a5c63e33fc
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Sun Jun 5 15:19:19 2022 -0700
    
        fix style
    
    commit a597ef63b899b3d5d277039341131458a17fc71d
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Sun Jun 5 15:19:14 2022 -0700
    
        create pages, use layouts, fix code style
    
    commit 92bd348bc3cd90f1a12195525739cc4f1f04dbbe
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Sun Jun 5 15:18:22 2022 -0700
    
        add navbar, layouts
    
    commit 3c712d7f686be28e401215044c4506bb3544d6d7
    Author: Adam Hultman <38937780+ACHultman@users.noreply.github.com>
    Date:   Sun Jun 5 15:17:17 2022 -0700
    
        add react icons lib

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -1,11 +1,13 @@
 import { VStack, Heading } from "@chakra-ui/react";
 import CoursesPreferencesTable from "../components/Preferences/CoursesPreferencesTable";
+import GeneralPreferences from "../components/Preferences/GeneralPreferences";
 
 const Preferences = () => {
     return (
         <VStack spacing={5} align="flex-start">
             <Heading>Prof Preferences</Heading>
             <CoursesPreferencesTable courses={["CSC225", "CSC226"]} />
+            <GeneralPreferences />
         </VStack>
     );
 };
