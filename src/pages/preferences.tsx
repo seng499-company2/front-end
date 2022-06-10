888ef3899818a886fa3433ac54246a6a971d52a7
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-06-10 15:42:08 -0700

    Squash merge

--- src/pages/preferences.tsx
+++ src/pages/preferences.tsx
@@ -2,12 +2,14 @@ import { Box, Heading } from "@chakra-ui/react";
 
 import Availability from "../components/Preferences/Availability";
 import CoursesPreferencesTable from "../components/Preferences/CoursesPreferencesTable";
+import GeneralPreferences from "../components/Preferences/GeneralPreferences";
 
 const Preferences = () => {
     return (
         <Box pt="1rem">
             <Heading>Prof Preferences</Heading>
             <Availability />
+            <GeneralPreferences />
             <CoursesPreferencesTable courses={["CSC225", "CSC226"]} />
         </Box>
     );
