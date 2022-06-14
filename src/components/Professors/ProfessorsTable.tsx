261056f482a631d2b43f9b536e6865ed6bddda9e
Damandeep Singh Bawa <damandeep2018@gmail.com>
2022-06-14 12:04:26 -0700

    refactored prof table

--- src/components/Professors/ProfessorsTable.tsx
+++ src/components/Professors/ProfessorsTable.tsx
@@ -3,45 +3,43 @@ import { Button } from "@chakra-ui/react";
 import C2Table from "../C2Table";
 import { CompleteStatusBadge } from "../CompleteStatusBadge";
 
-const ProfessorsTable = (props) => {
-    const { professors, openDetails } = props;
-
-    const columns = [
-        {
-            Header: "Name",
-            accessor: "name",
-        },
-        {
-            Header: "Type",
-            accessor: "type",
-        },
-        {
-            Header: "Form Completed",
-            accessor: "status",
-            disableFilterBy: true,
-        },
-        {
-            Header: "",
-            accessor: "details",
-            disableSortBy: true,
-            disableFilterBy: true,
-        },
-    ];
-
-    const tableData = makeTableData(professors, openDetails);
-
-    return <C2Table columns={columns} entries={tableData} />;
-};
-
-const makeTableData = (professors, openDetails) => {
-    return professors.map((prof) => {
-        return {
-            name: prof.name,
-            type: prof.type,
-            status: <CompleteStatusBadge complete={prof.complete} />,
-            details: <Button variant="ghost" onClick={() => openDetails(prof)}> <ChevronRightIcon ml={1} w={5} h={5} /> </Button>,
-        };
-    });
+const ProfessorsTable = ({ professors, openDetails }) => {
+    return (
+        <C2Table
+            columns={[
+                {
+                    Header: "Name",
+                    accessor: "name",
+                },
+                {
+                    Header: "Type",
+                    accessor: "type",
+                },
+                {
+                    Header: "Form Completed",
+                    accessor: "status",
+                    disableFilterBy: true,
+                },
+                {
+                    Header: "",
+                    accessor: "details",
+                    disableSortBy: true,
+                    disableFilterBy: true,
+                },
+            ]}
+            entries={professors.map((prof) => ({
+                name: prof.name,
+                type: prof.type,
+                status: <CompleteStatusBadge complete={prof.complete} />,
+                details: (
+                    <Button variant="ghost" onClick={() => openDetails(prof)}>
+                        {" "}
+                        <ChevronRightIcon ml={1} w={5} h={5} />{" "}
+                    </Button>
+                ),
+            }))}
+        />
+    );
 };
 
 export default ProfessorsTable;
