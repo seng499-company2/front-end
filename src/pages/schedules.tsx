6ce6983e7119874d0bbd748d1da948ce485521ba
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-26 20:39:50 -0700

    add generate api call

--- src/pages/schedules.tsx
+++ src/pages/schedules.tsx
@@ -1,13 +1,31 @@
-import { Center, Button, Progress } from "@chakra-ui/react";
-import { ReactElement, useState } from "react";
+import { Center, Button, Text, VStack } from "@chakra-ui/react";
+import { ReactElement } from "react";
 
 import AdminLayout from "../components/Layout/AdminLayout";
+import useGetQuery from "../utils/useGetQuery";
 
 const Schedules = ({ scheduledCourses }) => {
+    const { data, isLoading, isError, execute } = useGetQuery(
+        "/schedule/2022/FALL/2",
+        {
+            manual: true,
+        }
+    );
+
     return (
         <Center height="50vh">
-            <Button>Generate Schedule</Button>
-            <Progress size="xs" isIndeterminate />
+            <VStack gap={4}>
+                <Button
+                    onClick={() => {
+                        execute();
+                    }}
+                    isLoading={isLoading}
+                >
+                    Generate Schedule
+                </Button>
+                <Text>{data}</Text>
+                {isError && <Text color="red">Error</Text>}
+            </VStack>
         </Center>
     );
 };
