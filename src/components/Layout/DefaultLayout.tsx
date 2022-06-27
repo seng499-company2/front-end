b95514c048756587c5f6a1e2f3114e93e38b64ad
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-06-26 23:10:59 -0700

    add login functionality to default layout

--- src/components/Layout/DefaultLayout.tsx
+++ src/components/Layout/DefaultLayout.tsx
@@ -5,19 +5,25 @@ import {
     useColorModeValue,
     VStack,
 } from "@chakra-ui/react";
+
+import useAuth from "@hooks/useAuth";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 
-const DefaultLayout = ({ children }) => (
-    <VStack bg={useColorModeValue("background.main", "gray.900")}>
-        <HStack ml="auto" pt={5} pr={5}>
-            <DarkModeSwitch />
-            <Button>Logout</Button>
-        </HStack>
+const DefaultLayout = ({ children }) => {
+    const { logout } = useAuth();
 
-        <Flex minH={"100vh"} justify={"center"} padding={10} width="100%">
-            {children}
-        </Flex>
-    </VStack>
-);
+    return (
+        <VStack bg={useColorModeValue("background.main", "gray.900")}>
+            <HStack ml="auto" pt={5} pr={5}>
+                <DarkModeSwitch />
+                <Button onClick={() => logout()}>Logout</Button>
+            </HStack>
+
+            <Flex minH={"100vh"} justify={"center"} padding={10} width="100%">
+                {children}
+            </Flex>
+        </VStack>
+    );
+};
 
 export default DefaultLayout;
