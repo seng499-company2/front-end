d342cd87bad7a84015f1910c9f73d7216a08edb8
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-07-06 18:47:36 -0700

    add user context

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,7 +1,30 @@
 import axios from "axios";
 
-import { User } from "@hooks/useAuth";
 import { getToken } from "./auth";
+import { decodeJwt, JWTPayload } from "jose";
+
+type UserPayload = JWTPayload & {
+    email: string;
+    exp: number;
+    first_name: string;
+    iat: number;
+    is_superuser: boolean;
+    jti: string;
+    last_name: string;
+    refresh_exp: number;
+    token_type: string;
+    user_id: number;
+    username: string;
+};
+
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    isAdmin: boolean;
+}
 
 export async function getCurrentUser(): Promise<User> {
     const token = getToken();
@@ -18,4 +41,24 @@ export async function getCurrentUser(): Promise<User> {
     }
 
     return response.data;
+}
+
+export function getUserClaims(): User {
+    const token = getToken();
+
+    if (!token) {
+        return undefined;
+    }
+
+    // @ts-ignore
+    const decoded: UserPayload = decodeJwt(token);
+
+    return {
+        id: decoded.user_id,
+        firstName: decoded.first_name,
+        lastName: decoded.last_name,
+        email: decoded.email,
+        username: decoded.username,
+        isAdmin: decoded.is_superuser,
+    };
 }
