5f152f94f5b5fb0fdc83aa49ea58fa748aed2c76
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-07-25 14:55:04 -0700

    adjust color hash

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -6,15 +6,15 @@ import ColorHash from "color-hash";
 // lightness percent (100% is white, 0% is black, 50% is 'normal')
 const colorHash = {
     light: new ColorHash({
-        lightness: [0.35, 0.5, 0.65],
+        lightness: [0.35, 0.5, 0.6],
         hue: [
-            { min: 10, max: 20 }, // remove this line if colors are too brown
+            //{ min: 0, max: 30 }, // remove this line if colors are too brown
             { min: 30, max: 90 },
-            { min: 180, max: 210 },
-            { min: 270, max: 285 },
+            { min: 90, max: 270 },
+            { min: 270, max: 330 },
             { min: 330, max: 360 },
         ],
-        saturation: [0.6, 0.5, 0.4],
+        saturation: [0.55, 0.5, 0.4],
     }),
     dark: new ColorHash({
         lightness: [0.35, 0.4],
